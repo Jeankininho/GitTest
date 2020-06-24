12e5ea92f90c44b9250aa0d6bacb94ada2191b6f
Jeanderson Machado <jean.machado@skysoftware.com.br>
2020-06-24 14:26:47 -0300

    Incluido pessoa

---
+++ GitTest/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    public class Person
+    {
+
+        public Person()
+        {
+
+        }
+
+
+        public void Run()
+        {
+
+        }
+
+        public void Walk()
+        {
+
+        }
+
+    }
+
+}
