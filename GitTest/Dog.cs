6f728cc28a958b391ed9fc813730aba9f6e66b5a
Jeanderson Machado <jean.machado@skysoftware.com.br>
2020-06-24 15:14:59 -0300

    nova classe de cachorro

---
+++ GitTest/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    public class Dog
+    {
+        public void Bark()
+        {
+
+        }
+
+        public void Sleep()
+        {
+
+        }
+
+        public void Eat()
+        {
+
+        }
+    }
+}
